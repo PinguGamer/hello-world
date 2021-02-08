c940faa949d7ce792e5f5e84b389a49f4057bae8
Eduardo Santos Barreto <eduwagzozo@gmail.com>
2021-02-08 09:44:58 -0300

    Comando Atualizar corrigido
    erro de indentação não existe mais

--- botfcb.py
+++ botfcb.py
@@ -335,18 +335,18 @@ async def teste(ctx):
         print(roles)
 
 
-  @commands.is_owner()
-  @bot.command()
-  async def atualizar(ctx):
-      await ctx.message.delete()
-      os.chdir('..')
-      os.system('sudo rm -R Zapper-Bot')
-      os.system(
-          'git clone https://github.com/Zapper0/Zapper-bot.git'
-      )
-      os.chdir('Zapper-Bot')
-      os.system('clear')
-      os.system('python3 botfcb.py')
-      quit()
+@commands.is_owner()
+@bot.command()
+async def atualizar(ctx):
+    await ctx.message.delete()
+    os.chdir('..')
+    os.system('sudo rm -R Zapper-Bot')
+    os.system(
+        'git clone https://github.com/Zapper0/Zapper-bot.git'
+    )
+    os.chdir('Zapper-Bot')
+    os.system('clear')
+    os.system('python3 botfcb.py')
+    quit()
 
 bot.run("Nzc5MzM3MjcxMDU1NzQ1MDI1.X7fEZA.3SLth7ufSMICvS9mo5zBtNhF_Ys")
